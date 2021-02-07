7880e5b6c5c339790dde8a3f8b3eed884c0dffa5
Justin Doh <jrd224@lehigh.edu>
2021-02-07 15:53:22 -0500

    Edited Header

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,17 @@
 # R script to accompany Intro to R
+# 
+# Written by Troy Adair
+#
 # Copy for Justin Doh
+#
+# Customized by Justin Doh
 ######################################################
 
-# You will see that you can put non-executable "comment" lines
-# in your script by putting "#" at the beginning of the lines.
-# 
-# In fact, you can put comments anywhere on a line as long as 
-# you put "#" before them.
+# Assignment Examples
+# Numeric
+x = 7
+y <- 8
+
+
+# Character
+Name <- "Justin"
